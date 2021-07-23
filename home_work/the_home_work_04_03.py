8c5d5af7e901500cf4ef46ea92f1a76ee33ecf21
Barsty <barstyle@mail.ru>
2021-07-24 00:41:07 +0300

    4 ДЗ - 3-ая задача - пора бы остановиться

--- home_work/the_home_work_04_03.py
+++ home_work/the_home_work_04_03.py
@@ -2,22 +2,39 @@
 В приложении заданы словари с названиями, рейтингами и жанрами сериалов shows и ratings.
 Напишите программу, которая рассчитывает средний рейтинг для сериалов с жанром “фантастика”.
 '''
+
 # импорт из сущетсвующей уже базы
 from bd_for_hw_04 import shows, ratings
 
-average_list = []
 
-for x in shows.keys():
-    if x in ratings.keys():
-        if shows.setdefault(x) == 'фантастика':
-            average_list.append(ratings.setdefault(x))
-        print(average_list)
+# создаем функцию которая считает средний рейтинг по жанру
+def is_which_ratio(genre):
+    # список для хранения рейтингов
+    average_list = []
 
-average_ratio = 0
+    # заполняем список рейтингами конкретного жанра
+    for x in shows.keys():
+        if x in ratings.keys():
+            if shows.setdefault(x) == genre:
+                average_list.append(ratings.setdefault(x))
 
-for i in average_list:
-    average_ratio = average_ratio + i
+    # создаем переменную с нулевым значением, чтоб посчитать сумму ретинга
+    average_ratio = 0
 
-result = average_ratio/len(average_list)
+    # в этом циклен считаем сумму рейтингов
+    for i in average_list:
+        average_ratio = average_ratio + i
 
-print(result)
+    # вычисляем среднее значение
+    result = average_ratio / len(average_list)
+
+    # закрепялем результат с пояснительной бригадой для красоты
+    return ('{}{}{}{}'.format('Среднее занчение ретинга фильмов жанра - ', genre,
+                             ' - ', round(result, 2)))
+
+
+
+print(is_which_ratio('фантастика'))
+print(is_which_ratio('криминал'))
+print(is_which_ratio('драма'))
+print(is_which_ratio('фэнтази'))
