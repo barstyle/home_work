81df6cde9502895d14d52bb7d301e9255ab8a534
Barsty <barstyle@mail.ru>
2021-07-26 00:01:55 +0300

    4 ДЗ - Задание 4 - Почти готово

--- home_work/the_home_work_04_04.py
+++ home_work/the_home_work_04_04.py
@@ -19,9 +19,27 @@ from bd_for_hw_04 import shows, ratings
 # создаем пустой список, в котором будем хранить сериалы подхлдящие для Васи
 shows_for_vasya = []
 
-# Генериркем этот список
+# генерируем этот список
 for film in shows:
     if shows.setdefault(film) != 'фэнтази' and ratings.setdefault(film) > 0.85:
         shows_for_vasya.append(film)
 
-print(shows_for_vasya)
+# функция подбирает случайный сериал подходящий Васе
+def is_what_show():
+    i = random.randint(0, len(shows_for_vasya)-1)
+    show = shows_for_vasya[i]
+    return show
+
+will = True
+
+while will:
+    show = is_what_show()
+    print('Хотите посмотреть - {}?'
+          '\nЕго рейтинг - {}'.format(show, ratings.setdefault(show)))
+    click = input('Если хотите продолжить подборку, нажмите - "Y"'
+                  '\nЕсли хотите остановть подборку, нажмите любую дргую кнопку: ')
+    if click.lower() == 'y':
+        print('')
+        continue
+    else:
+        break
