2b36a02c0f4b4117defa65a868d4568e10819e9c
Barsty <barstyle@mail.ru>
2021-08-19 23:48:43 +0300

    8 - ДЗ - 1 задание, готово

--- home_work/home_work_08/the_home_work_08_01.py
+++ home_work/home_work_08/the_home_work_08_01.py
@@ -12,10 +12,8 @@
 import requests, re
 from bs4 import BeautifulSoup
 
-
 # С помощью библиотек requests прочитайте содержимое страницы
 r = requests.get('https://store.steampowered.com/genre/Free%20to%20Play/')
-
 content = r.text
 
 # С помощью библиотек bs4 прочитайте содержимое страницы
@@ -30,17 +28,9 @@ tag_free_to_play = soup.find_all('a')
 for i in tag_free_to_play:
     # и где встречается нужный нам текст
     if 'Free To Play' in i.text:
-        # print(i)
-        print(i.find(class_='tab_item_name').text)
-        # print(i.find(class_='discount_final_price').text)
-        # print(i.find('a'))
-        print(i.get('href'))
         # добавляем в словарь
         free_to_play_dict[i.find(class_='tab_item_name').text] = i.get('href')
-        print('-'*140)
-
-
-# https://www.youtube.com/watch?v=7hn1_t2ZtJQ
 
 # выводим словарь на экран
-print(free_to_play_dict)
+for i in free_to_play_dict:
+    print(i, '-', free_to_play_dict[i])
